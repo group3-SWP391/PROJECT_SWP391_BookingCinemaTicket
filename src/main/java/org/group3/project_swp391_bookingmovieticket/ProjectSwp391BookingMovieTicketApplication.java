8fbf6dc9836a23875d35952f54b1a643002ed170
hieuLeFpt <lehieuhn153@gmail.com>
2025-05-20 00:08:37 +0700

    initialize project
    create dependencies for the project

---
+++ src/main/java/org/group3/project_swp391_bookingmovieticket/ProjectSwp391BookingMovieTicketApplication.java
@@ -0,0 +1,13 @@
+package org.group3.project_swp391_bookingmovieticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSwp391BookingMovieTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSwp391BookingMovieTicketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/group3/project_swp391_bookingmovieticket/ProjectSwp391BookingMovieTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.group3.project_swp391_bookingmovieticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSwp391BookingMovieTicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
