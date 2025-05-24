cb992bac3c1fb180055af9b44157d0faa9e73bd4
hieuLeFpt <lehieuhn153@gmail.com>
2025-05-24 14:56:24 +0700

    Login-Logout-HelloUser-FormatTheme

---
+++ src/main/java/org/group3/project_swp391_bookingmovieticket/entities/Role.java
@@ -0,0 +1,16 @@
+package org.group3.project_swp391_bookingmovieticket.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+}
