5cbd04baee60c578fb56cbc2664b2378883c0f43
DoHuyThanh <thanhdhhe180811@fpt.edu.vn>
2025-05-25 23:19:32 +0700

    Test Login

---
+++ src/main/java/org/group3/project_swp391_bookingmovieticket/repositories/IUserRepository.java
@@ -0,0 +1,10 @@
+package org.group3.project_swp391_bookingmovieticket.repositories;
+
+import org.group3.project_swp391_bookingmovieticket.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface IUserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByPhoneAndPassword(String phone, String password);
+}
---
+++ src/main/java/org/group3/project_swp391_bookingmovieticket/services/impl/UserService.java
@@ -0,0 +1,52 @@
+package org.group3.project_swp391_bookingmovieticket.services.impl;
+
+import org.group3.project_swp391_bookingmovieticket.entities.User;
+import org.group3.project_swp391_bookingmovieticket.repositories.IUserRepository;
+import org.group3.project_swp391_bookingmovieticket.services.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService implements IUserService {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> findById(Integer id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public void update(User user) {
+
+    }
+
+    @Override
+    public void remove(Integer id) {
+
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> findByUsernameAndPassword(String username, String password) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> findByPhoneAndPassword(String username, String password) {
+        return userRepository.findByPhoneAndPassword(username, password);
+    }
+}
