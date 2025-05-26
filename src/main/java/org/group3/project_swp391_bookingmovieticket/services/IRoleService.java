8eedc60e1ed7f977eecbc9854ccd91799024dcf2
PhuPham70 <phupdhe181976@fpt.edu.vn>
2025-05-26 13:46:12 +0700

    test demo

---
+++ src/main/java/org/group3/project_swp391_bookingmovieticket/services/IRoleService.java
@@ -0,0 +1,7 @@
+package org.group3.project_swp391_bookingmovieticket.services;
+
+import org.group3.project_swp391_bookingmovieticket.entities.Role;
+
+public interface IRoleService extends IGeneralService<Role>{
+
+}
