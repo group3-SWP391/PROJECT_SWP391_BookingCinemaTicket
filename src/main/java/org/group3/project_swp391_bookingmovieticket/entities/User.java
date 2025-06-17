2923b3e603d0cb9121ed91671819da66a61c6d8a
PhuPham70 <phupdhe181976@fpt.edu.vn>
2025-06-17 21:54:08 +0700

    the first commint with role staff

---
+++ src/main/java/org/group3/project_swp391_bookingmovieticket/entities/User.java
@@ -0,0 +1,35 @@
+package org.group3.project_swp391_bookingmovieticket.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "[user]")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullname;
+
+    @Column(name = "phone", nullable = true)
+    private String phone;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+
+}
